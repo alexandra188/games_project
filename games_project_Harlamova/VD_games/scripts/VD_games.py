5052bd6752a98d341d9200b9ccfca9f20306509a
alexandra188 <alex2008alexx@gmail.com>
2026-02-18 01:17:34 +0300

    Практическая работа №1: настройка проекта

---
+++ games_project_Harlamova/VD_games/scripts/VD_games.py
@@ -0,0 +1,5 @@
+def greet():
+    print('Welcome to my application!')
+
+if __name__ == "__main__":
+    greet()
---
+++ games_project_Harlamova/VD_games/scripts/VD_main.py
@@ -0,0 +1,9 @@
+from VD_games.scripts.VD_games import greet
+
+def main():
+    print("Запуск приложения...")
+    greet()
+    print("Приложение завершено.")
+
+if __name__ == "__main__":
+    main()
